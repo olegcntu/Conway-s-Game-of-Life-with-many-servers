87508c1bd55f708aa3a34fdf2b21b62ea5d69a01
olegcntu <oleg.krasavcik.41@gmail.com>
2021-12-16 22:26:41 +0200

    all work, without server and threads

--- src/main/java/MatrixWork.java
+++ src/main/java/MatrixWork.java
@@ -1,16 +1,80 @@
 public class MatrixWork {
-   private int matrix[][];
-   private int count;
+    private int matrix[][];
+    private int count;
 
-    public MatrixWork(int matrix[][],int count) {
-        this.matrix=matrix;
-        this.count=count;
-
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < count; j++) {
-                System.out.print(matrix[i][j]);
-            }
-            System.out.println();
-        }
+    public MatrixWork(int matrix[][], int count) {
+        this.matrix = matrix;
+        this.count = count;
     }
+
+    public int[][] matrixTransformation() {
+
+        for (int i = 1; i < count + 1; i++) {
+            for (int j = 1; j < count + 1; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix = newCell(i, j);
+                }
+                if (matrix[i][j] == 1) {
+                    matrix = dyingCellOrLive(i, j);
+                }
+
+            }
+
+        }
+
+        return matrix;
+    }
+
+    private int[][] newCell(int i, int j) {
+
+        int cellCont = sumCells(i, j);
+        if (cellCont == 3) {
+            matrix[i][j] = 2;
+        }
+
+        return matrix;
+    }
+
+
+    private int[][] dyingCellOrLive(int i, int j) {
+
+        int cellCont = sumCells(i, j);
+        if (cellCont == 3 || cellCont == 2) {
+            matrix[i][j] = 1;
+        }
+        else matrix[i][j] = 3;
+
+        return matrix;
+    }
+
+    private int sumCells(int i, int j) {
+        int sum = 0;
+        if (matrix[i - 1][j - 1] == 1 || matrix[i - 1][j - 1] == 3) {
+            sum++;
+        }
+        if (matrix[i - 1][j] == 1 || matrix[i - 1][j] == 3) {
+            sum++;
+        }
+
+        if (matrix[i - 1][j + 1] == 1 || matrix[i - 1][j + 1] == 3) {
+            sum++;
+        }
+        if (matrix[i][j - 1] == 1 || matrix[i][j - 1] == 3) {
+            sum++;
+        }
+        if (matrix[i][j + 1] == 1 || matrix[i][j + 1] == 3) {
+            sum++;
+        }
+        if (matrix[i + 1][j - 1] == 1 || matrix[i + 1][j - 1] == 3) {
+            sum++;
+        }
+        if (matrix[i + 1][j] == 1 || matrix[i + 1][j]  == 3) {
+            sum++;
+        }
+        if (matrix[i + 1][j + 1] == 1 || matrix[i + 1][j + 1]  == 3) {
+            sum++;
+        }
+        return sum;
+    }
+
 }
--- src/main/java/UserForm.java
+++ src/main/java/UserForm.java
@@ -3,11 +3,15 @@ import javax.swing.border.Border;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.Thread.sleep;
 
 public class UserForm extends JFrame {
     private JPanel mainPanel;
     private JScrollPane scroll;
     private JButton button2;
+    private JButton runButton;
     private final JPanel sc;
     private final int count = 10;
     private int[][] matrix;
@@ -30,25 +34,64 @@ public class UserForm extends JFrame {
         button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                Component[] components = sc.getComponents();
-                matrix = new int[count][count];
-                int a = 0;
-                for (int i = 0; i < count; i++) {
-                    for (int j = 0; j < count; j++)
-                        if (components[a++].getBackground().equals(Color.BLACK)) {
-                            matrix[i][j] = 1;
-                        } else matrix[i][j] = 0;
-                }
-
-                MatrixWork matrixWork=new MatrixWork(matrix,count);
+                fromTableToMatrix();
 
             }
 
+        });
 
+
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                    fromTableToMatrix();
+
+            }
         });
     }
 
+    private void fromTableToMatrix() {
+        Component[] components = sc.getComponents();
+        matrix = new int[count + 2][count + 2];
+        int countOfComponent = 0;
+        for (int i = 0; i < count + 2; i++) {
+            for (int j = 0; j < count + 2; j++) {
+
+                if (i == 0 || j == 0 || i == count + 1 || j == count + 1) {
+                    matrix[i][j] = 0;
+                    continue;
+                }
+                if (components[countOfComponent++].getBackground().equals(Color.BLACK)) {
+                    matrix[i][j] = 1;
+                } else matrix[i][j] = 0;
+            }
+        }
+        MatrixWork matrixWork = new MatrixWork(matrix, count);
+        matrix = matrixWork.matrixTransformation();
+        fromMatrixToTable(components);
+    }
+
+
+    private void fromMatrixToTable(Component[] components) {
+
+        int index = 0;
+        for (int i = 1; i < count + 1; i++) {
+            for (int j = 1; j < count + 1; j++) {
+
+                if (matrix[i][j] == 3 || matrix[i][j] == 0) {
+                    components[index].setBackground(Color.WHITE);
+                } else {
+                    components[index].setBackground(Color.BLACK);
+                }
+                sc.setComponentZOrder(components[index], 99);
+
+                index++;
+            }
+        }
+
+
+    }
 
     public static class TestPane extends JPanel {
 
@@ -64,7 +107,7 @@ public class UserForm extends JFrame {
                     gbc.gridx = col;
                     gbc.gridy = row;
                     CellPane cellPane = new CellPane();
-                    Border border ;
+                    Border border;
                     border = new MatteBorder(1, 1, 1, 1, Color.GRAY);
 
                     cellPane.setBorder(border);
@@ -84,9 +127,7 @@ public class UserForm extends JFrame {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if (getBackground() == Color.BLACK) setBackground(Color.WHITE);
-                    else {
-                        setBackground(Color.BLACK);
-                    }
+                    else setBackground(Color.BLACK);
                 }
 
             });
